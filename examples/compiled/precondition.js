82ef0071dc7620c7e63ddcb7aa40ddf930313cfa
Charles Pick <charles.pick@gmail.com>
2014-04-21 19:35:08 +0100

    remove existing files

--- examples/compiled/precondition.js
+++
@@ -1,5 +0,0 @@
-var OBLIGATIONS = require('obligations');
-function warn(message) {
-    OBLIGATIONS.precondition(typeof message === 'string');
-    alert('Warning!\n' + message);
-}
--- examples/src/precondition.js
+++
@@ -1,6 +0,0 @@
-function warn (message) {
-  pre:
-    typeof message === 'string';
-  main:
-    alert('Warning!\n' + message);
-}
